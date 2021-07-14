630deb9e7633f66b848aca03138f297185243eae
Furkan KADIOGLU <iam@frkn.com.tr>
2021-07-15 02:21:56 +0300

    Renames first test

--- tests/Engines/test_base_engine.py
+++ tests/Engines/test_base_engine.py
@@ -2,6 +2,6 @@ from shorty.Engines.BaseEngine import BaseEngine
 
 class TestBaseEngine:
     
-    def test_base_engine_api_endpoint(self):
+    def test_base_engine_name(self):
         engine = BaseEngine()
         assert engine.name == ""
