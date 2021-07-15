ee18c05e4cbe9e8b366b14abdd7e898a10f34b09
Furkan KADIOGLU <iam@frkn.com.tr>
2021-07-16 02:20:03 +0300

    BitlyResponse, handleResponse, prepareRequest base functions

--- shorty/Providers/BitlyProvider.py
+++ shorty/Providers/BitlyProvider.py
@@ -1,4 +1,5 @@
 from shorty.Providers.BaseProvider import BaseProvider
+from shorty.Responses.BitlyResponse import BitlyResponse
 import os
 class BitlyProvider(BaseProvider):
     requestUrl = "https://api-ssl.bitly.com/v4/shorten"
@@ -11,3 +12,27 @@ class BitlyProvider(BaseProvider):
 
     def getAuthenticationToken(self):
         return os.environ.get('BITLY_AUTH_KEY')
+
+    def handleResponse(self, response):
+        responseStructure = BitlyResponse()
+        apiResponse = responseStructure.handle(response.content)
+        return apiResponse
+
+    def prepareRequest(self, url):
+
+        headers = {
+            'Authorization': f'Bearer {self.getAuthenticationToken()}',
+            'Content-Type': 'application/json',
+        }
+
+        data = { 
+            "long_url": url, 
+            "domain": "bit.ly" 
+        }
+
+        return {
+            "headers": headers,
+            "json": data,
+            "params": None,
+            "type": self.type
+        }
