f0fe0aa05d78fab4cbc93b64a6d21c0e00b52160
Furkan KADIOGLU <iam@frkn.com.tr>
2021-07-15 05:56:43 +0300

    Second provider (tinyurl)
    Created for fallback scenario

---
+++ tests/Providers/test_tinyurl_provider.py
@@ -0,0 +1,18 @@
+from shorty.Providers.TinyUrlProvider import TinyUrlProvider
+import os
+class TestTinyurlProvider:
+    
+    provider = TinyUrlProvider()
+
+    def test_base_provider_variables(self):
+        assert self.provider.requestUrl == "https://tinyurl.com/api-create.php"
+        assert self.provider.type == "TEXT"
+        assert self.provider.method == "GET"
+        assert self.provider.name == "TinyUrl"
+
+    def test_get_request_url(self):
+        assert self.provider.requestUrl == "https://tinyurl.com/api-create.php"
+        assert self.provider.getEndpoint() == "https://tinyurl.com/api-create.php"
+        assert self.provider.getRequestUrl() == "https://tinyurl.com/api-create.php"
+
+
